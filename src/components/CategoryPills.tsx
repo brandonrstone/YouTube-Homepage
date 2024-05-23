899366303ee1f611dc3250a652762dbc1af6c3a5
Brandon Stone <brandonrstone@gmail.com>
2024-05-22 23:55:21 -0400

    Added grid & pill section

--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { ChevronLeft, ChevronRight } from 'lucide-react'
 
 import Button from './Button'
@@ -9,13 +9,39 @@ type CategoryPillsProps = {
   onSelect: (category: string) => void
 }
 
+const TRANSLATE_AMOUNT = 200
+
 export default function CategoryPills({ categories, selectedCategory, onSelect }: CategoryPillsProps) {
+  const [translate, setTranslate] = useState(0)
   const [isLeftVisible, setIsLeftVisible] = useState(true)
   const [isRightVisible, setIsRightVisible] = useState(true)
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  // Resize observer to monitor the left/right buttons for the pills
+  useEffect(() => {
+    if (containerRef.current == null) return
+
+    const observer = new ResizeObserver(entries => {
+      const container = entries[0]?.target // Same as containerRef.current in thise case
+      if (container == null) return
+
+      setIsLeftVisible(translate > 0)
+      setIsRightVisible(translate + container.clientWidth < container.scrollWidth)
+    })
+
+    observer.observe(containerRef.current)
+    return () => observer.disconnect()
+  }, [categories, translate])
 
   return (
-    <div className='relative overflow-x-hidden'>
-      <div className='w-[max-content] flex whitespace-nowrap gap-3 transition-transform'>
+    <div
+      className='relative overflow-x-hidden'
+      ref={containerRef}
+    >
+      <div
+        className='w-[max-content] flex whitespace-nowrap gap-3 transition-transform'
+        style={{ transform: `translateX(-${translate}px)` }}
+      >
         {categories.map(category => (
           <Button
             key={category}
@@ -30,9 +56,16 @@ export default function CategoryPills({ categories, selectedCategory, onSelect }
       {isLeftVisible && (
         <div className='w-24 h-full absolute left-0 top-1/2 bg-gradient-to-r from-white from-50% to-transparent -translate-y-1/2'>
           <Button
-            className='w-auto h-full aspect-square'
+            className='w-auto h-full aspect-square p-1.5'
             size='icon'
             variant='ghost'
+            onClick={() => {
+              setTranslate(translate => {
+                const newTranslate = translate - TRANSLATE_AMOUNT
+                if (newTranslate <= 0) return 0
+                return newTranslate
+              })
+            }}
           >
             <ChevronLeft />
           </Button>
@@ -41,9 +74,19 @@ export default function CategoryPills({ categories, selectedCategory, onSelect }
       {isRightVisible && (
         <div className='w-24 h-full absolute right-0 flex justify-end top-1/2 bg-gradient-to-l from-white from-50% to-transparent -translate-y-1/2'>
           <Button
-            className='w-auto h-full aspect-square'
+            className='w-auto h-full aspect-square p-1.5'
             size='icon'
             variant='ghost'
+            onClick={() => setTranslate(translate => {
+              console.log(containerRef)
+              console.log(translate)
+              if (containerRef.current == null) return translate
+              const edge = containerRef.current.scrollWidth // Full scrollable width
+              const width = containerRef.current.clientWidth // Visible width; amount shown on screen
+              const newTranslate = translate + TRANSLATE_AMOUNT
+              if (newTranslate + width >= edge) return edge - width
+              return newTranslate
+            })}
           >
             <ChevronRight />
           </Button>
---
+++ src/components/VideoGridItem.tsx
@@ -0,0 +1,88 @@
+import { useEffect, useRef, useState } from 'react'
+import { formatDuration } from '../utils/formatDuration'
+import { formatTimeAgo } from '../utils/formatTimeAgo'
+
+type VideoGridItemProps = {
+  id: string
+  title: string
+  channel: {
+    id: string
+    name: string
+    profileUrl: string
+  }
+  views: number
+  postedAt: Date
+  duration: number
+  thumbnailUrl: string
+  videoUrl: string
+}
+
+const VIEW_FORMATTER = new Intl.NumberFormat(undefined, { notation: 'compact' })
+
+export default function VideoGridItem({
+  id,
+  title,
+  channel,
+  views,
+  postedAt,
+  duration,
+  thumbnailUrl,
+  videoUrl
+}: VideoGridItemProps) {
+  const [isVideoPlaying, setIsVideoPlaying] = useState(false)
+  const videoRef = useRef<HTMLVideoElement>(null)
+
+  useEffect(() => {
+    if (videoRef.current == null) return
+
+    if (isVideoPlaying) {
+      videoRef.current.currentTime = 0
+      videoRef.current.play()
+    } else {
+      videoRef.current.pause()
+    }
+  }, [isVideoPlaying])
+
+  return (
+    <div
+      className='flex flex-col gap-2'
+      onMouseEnter={() => setIsVideoPlaying(true)}
+      onMouseLeave={() => setIsVideoPlaying(false)}
+    >
+      <a className='relative aspect-video' href={`/watch?v=${id}`}>
+        <img
+          className={`block w-full h-full object-cover transition-[border-radius] duration-200 ${isVideoPlaying ? 'rounded-0' : 'rounded-xl'}`}
+          src={thumbnailUrl}
+          alt='Video thumbnail'
+
+        />
+        <div className='absolute bottom-1 px-0.5 bg-secondary-dark text-secondary text-sm rounded'>
+          {formatDuration(duration)}
+        </div>
+        <video
+          className={`block h-full absolute inset-0 object-cover duration-200 delay-200 ${isVideoPlaying ? 'opacity-100' : 'opacity-0'}`}
+          src={videoUrl}
+          ref={videoRef}
+          muted
+          playsInline
+        />
+      </a>
+      <div className='flex gap-2'>
+        <a className='flex-shrink-0' href={`/@${channel.id}`}>
+          <img className='w-12 h-12 rounded-full' src={channel.profileUrl} />
+        </a>
+        <div className='flex flex-col'>
+          <a className='font-bold' href={`/watch?v=${id}`}>
+            {title}
+          </a>
+          <a className='text-secondary' href={`/@${channel.id}`}>
+            {channel.name}
+          </a>
+          <div className='text-secondary text-sm'>
+            {VIEW_FORMATTER.format(views)} Views • {formatTimeAgo(postedAt)}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
